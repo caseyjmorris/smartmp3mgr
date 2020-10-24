4313f6499a921c2ba6275b0f3af97b02045839dd
Casey J. Morris <caseyjmorris@gmail.com>
2020-10-24 14:24:58 -0400

    move stuff out of fn

--- internal/record_keeping_test.go
+++ internal/record_keeping_test.go
@@ -6,34 +6,35 @@ import (
 	"testing"
 )
 
-func TestRecordAndFetchSongs(t *testing.T) {
-	records := []Song{{
-		Path:        "c:\\Users\\Casey\\Song1.mp3",
-		Artist:      "Starpoint",
-		Album:       "Restless",
-		Title:       "Object of My Desire",
-		Hash:        "adfd",
-		Genre:       "R&B",
-		AlbumArtist: "Starpoint",
-		TrackNumber: 1,
-		TotalTracks: 12,
-		DiscNumber:  1,
-		TotalDiscs:  1,
-	}, {
-		Path:        "c:\\Users\\Casey\\Song2.mp3",
-		Artist:      "浜崎あゆみ",
-		Album:       "オリアの木",
-		Title:       "オリアの木",
-		Hash:        "cfdkfkslafj",
-		Genre:       "J-Pop",
-		AlbumArtist: "浜崎あゆみ",
-		TrackNumber: 2,
-		TotalTracks: 2,
-		DiscNumber:  1,
-		TotalDiscs:  2,
-	}}
+var records = []Song{{
+	Path:        "c:\\Users\\Casey\\Song1.mp3",
+	Artist:      "Starpoint",
+	Album:       "Restless",
+	Title:       "Object of My Desire",
+	Hash:        "adfd",
+	Genre:       "R&B",
+	AlbumArtist: "Starpoint",
+	TrackNumber: 1,
+	TotalTracks: 12,
+	DiscNumber:  1,
+	TotalDiscs:  1,
+}, {
+	Path:        "c:\\Users\\Casey\\Song2.mp3",
+	Artist:      "浜崎あゆみ",
+	Album:       "オリアの木",
+	Title:       "オリアの木",
+	Hash:        "cfdkfkslafj",
+	Genre:       "J-Pop",
+	AlbumArtist: "浜崎あゆみ",
+	TrackNumber: 2,
+	TotalTracks: 2,
+	DiscNumber:  1,
+	TotalDiscs:  2,
+}}
 
-	const connectionString = "file:test.db?cache=shared&mode=memory"
+const connectionString = "file:test.db?cache=shared&mode=memory"
+
+func TestRecordAndFetchSongs(t *testing.T) {
 
 	db, err := sql.Open("sqlite3", connectionString)
 	if err != nil {
