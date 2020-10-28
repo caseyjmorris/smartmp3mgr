3b03860378faca32e9d1987909a6ba362392a72b
Casey J. Morris <caseyjmorris@gmail.com>
2020-10-27 21:12:54 -0400

    make it look like it's doing something.

--- go.mod
+++ go.mod
@@ -7,6 +7,9 @@ require (
 	github.com/dhowden/plist v0.0.0-20141002110153-5db6e0d9931a // indirect
 	github.com/dhowden/tag v0.0.0-20200828214007-46e57f75dbfc
 	github.com/mattn/go-sqlite3 v1.14.4
+	github.com/schollz/progressbar/v3 v3.6.2
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/sys v0.0.0-20201027140754-0fcbb8f4928c // indirect
 	golang.org/x/text v0.3.3 // indirect
 	gopkg.in/yaml.v2 v2.3.0
 )
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/caseyjmorris/smartmp3mgr/mp3"
 	"github.com/caseyjmorris/smartmp3mgr/records"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/schollz/progressbar/v3"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -52,6 +53,7 @@ func record() {
 	homeDir, _ := os.UserHomeDir()
 	defaultDb := filepath.Join(homeDir, ".smartmp3mgr.sql")
 	recordDb := recordCmd.String("dbPath", defaultDb, "path to sqlite db")
+	rehash := recordCmd.Bool("reparse", false, "force a rehash of reparse files")
 
 	_ = recordCmd.Parse(os.Args[2:])
 	info, err := os.Stat(*recordDir)
@@ -64,26 +66,46 @@ func record() {
 		os.Exit(1)
 	}
 
+	fmt.Printf("Scanning %q for MP3s\n", *recordDir)
 	mp3Files, err := files.FindMP3Files(*recordDir)
 
+	db, err := records.Open(*recordDb)
 	if err != nil {
-		fmt.Println(err)
+		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+
+	existing, err := db.FetchSongs()
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	existingMap := make(map[string]mp3.Song)
+
+	if !*rehash {
+		for _, existingFile := range existing {
+			existingMap[existingFile.Path] = existingFile
+		}
+	}
+
+	fmt.Printf("Scanning %d files\n", len(mp3Files))
+
+	bar := progressbar.Default(int64(len(mp3Files)))
 
 	var parsed []mp3.Song
 	for _, file := range mp3Files {
-		record, err := mp3.ParseMP3(file)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+		var record mp3.Song
+		if cached, ok := existingMap[file]; ok {
+			record = cached
+		} else {
+			record, err = mp3.ParseMP3(file)
+			if err != nil {
+				continue
+			}
 		}
 		parsed = append(parsed, record)
-	}
-	db, err := records.Open(*recordDb)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		_ = bar.Add(1)
 	}
 
 	tx, err := db.Begin()
@@ -93,12 +115,20 @@ func record() {
 		os.Exit(1)
 	}
 
+	fmt.Printf("Updating database at %q\n", *recordDb)
+
+	bar = progressbar.Default(int64(len(parsed)))
 	for _, parsedSong := range parsed {
+		if _, ok := existingMap[parsedSong.Path]; ok {
+			_ = bar.Add(1)
+			continue
+		}
 		err = db.RecordSong(parsedSong)
 		if err != nil {
 			_, _ = fmt.Fprintf(os.Stderr, "error saving %q:  %s\n", parsedSong.Path, err)
 			os.Exit(1)
 		}
+		_ = bar.Add(1)
 	}
 
 	err = tx.Commit()
@@ -121,7 +151,7 @@ func findNew() {
 	findNewCmd := flag.NewFlagSet("find-new", flag.ExitOnError)
 	newCmdDir := findNewCmd.String("directory", "", "directory")
 	newCmdDb := findNewCmd.String("dbPath", defaultDb, "path to sqlite db")
-	rehash := findNewCmd.Bool("rehash", false, "force a recalculation of existingFiles hashes")
+	rehash := findNewCmd.Bool("rehash", false, "force a recalculation of existing file hashes")
 	err := findNewCmd.Parse(os.Args[2:])
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "%s", err)
@@ -151,6 +181,8 @@ func findNew() {
 		os.Exit(1)
 	}
 
+	fmt.Printf("Looking for files in %q\n", *newCmdDir)
+
 	mp3Files, err := files.FindMP3Files(*newCmdDir)
 
 	if err != nil {
@@ -158,9 +190,13 @@ func findNew() {
 		os.Exit(1)
 	}
 
-	existingFiles, err := db.FetchSongs()
 	existsMap := make(map[string]mp3.Song)
 	if !*rehash {
+		fmt.Printf("Checking existing records in DB %q\n", *newCmdDb)
+		existingFiles, err := db.FetchSongs()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Error reading database:  %s\n", err)
+		}
 		for _, existingRecord := range existingFiles {
 			existsMap[existingRecord.Hash] = existingRecord
 		}
@@ -172,6 +208,11 @@ func findNew() {
 		_, _ = fmt.Fprintf(os.Stderr, "failed to start transaction:  %s\n", err)
 		os.Exit(1)
 	}
+
+	fmt.Printf("Hashing %d files and comparing against existing records in DB %q\n", len(mp3Files), *newCmdDb)
+
+	bar := progressbar.Default(int64(len(mp3Files)))
+	var results []string
 
 	for _, file := range mp3Files {
 		var hashS string
@@ -196,7 +237,8 @@ func findNew() {
 
 		if _, ok := existsMap[hashS]; !ok {
 			uniq++
-			fmt.Println(file)
+			results = append(results, file)
+			_ = bar.Add(1)
 		}
 	}
 
@@ -207,7 +249,11 @@ func findNew() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("(%d new songs)", uniq)
+	for _, result := range results {
+		fmt.Println(result)
+	}
+
+	fmt.Printf("(%d new songs)\n", uniq)
 
 	os.Exit(0)
 }
